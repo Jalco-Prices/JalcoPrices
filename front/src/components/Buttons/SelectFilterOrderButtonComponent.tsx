f9324839c0dfdeaa085fa9fdbca88f3e89912390
Alexme116 <alexmoncada16@hotmail.com>
2026-05-01 11:58:26 -0600

    Shadow added to Select Button Component and position fixed

--- front/src/components/Buttons/SelectFilterOrderButtonComponent.tsx
+++ front/src/components/Buttons/SelectFilterOrderButtonComponent.tsx
@@ -3,24 +3,46 @@ import "@/styles/Buttons/SelectFilterOrderButtonStyle.css"
 // Hooks
 import { useIsHoverDevice } from "@/hooks/useIsHoverDevice"
 // Utils
+import { useRef, useEffect, useState, useId } from "react"
 import { FilterIcon, SortDescendingIcon } from "@/icons/Icons"
 
 
 export default function SelectFilterOrderButtonComponent(
     { label, iconName, options, isShowingOptions, setIsShowingOptions, setSelectedOption }
     :
-    { readonly label: string, readonly iconName: string, readonly options: { label: string, value: string }[], readonly isShowingOptions: boolean, readonly setIsShowingOptions: (isShowing: boolean) => void, readonly setSelectedOption: (option: string | null) => void }
+    {
+        readonly label: string, readonly iconName: string, readonly options: { label: string, value: string }[], readonly isShowingOptions: boolean,
+        readonly setIsShowingOptions: (isShowing: boolean) => void, readonly setSelectedOption: (option: string | null) => void
+    }
 ) {
+    const [optionsWidth, setOptionsWidth] = useState<number | null>(null)
+
+    const optionsRef = useRef<HTMLDivElement>(null)
+    const uid = useId().replaceAll("-", "")
+
     const isHoverDevice = useIsHoverDevice()
+
+    useEffect(() => {
+        if (isShowingOptions && optionsRef.current) {
+            setOptionsWidth(optionsRef.current.offsetWidth)
+        } else {
+            setOptionsWidth(null)
+        }
+    }, [isShowingOptions])
 
     return (
         <>
+            {optionsWidth && (
+                <style>{`#${uid}.select-filter-order-button.open { width: ${optionsWidth}px; }`}</style>
+            )}
+
             <section
                 className="select-filter-order-section-container"
                 onMouseLeave={isHoverDevice ? () => setIsShowingOptions(false) : undefined}
             >
                 {/* Select Button */}
                 <button
+                    id={uid}
                     className={`select-filter-order-button ${isShowingOptions ? 'open' : ''}`}
                     onClick={() => setIsShowingOptions(!isShowingOptions)}
                 >
@@ -36,7 +58,7 @@ export default function SelectFilterOrderButtonComponent(
 
                 {/* Options */}
                 {isShowingOptions && (
-                    <div className="select-filter-order-options-container">
+                    <div ref={optionsRef} className="select-filter-order-options-container">
                         {options.map((option) => (
                             <div
                                 key={option.value}
